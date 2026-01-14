c5561680043f871bb16ca5c3f2a445da1adc5af4
AiranCana <airancc2005@gmail.com>
2026-01-14 14:16:08 +0100

    hasta ft_memmove

---
+++ ft_substr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acanadil <acanadil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/14 12:29:30 by acanadil          #+#    #+#             */
+/*   Updated: 2026/01/14 12:52:29 by acanadil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*leter;
+	int		count;
+
+	count = 0;
+	leter = malloc(sizeof (char) * len);
+	if (!leter)
+	{
+		return (NULL);
+	}
+	s += start;
+	while (count++ != len)
+	{
+		*leter = *s;
+		++leter;
+		++s;
+	}
+	*leter = '\0';
+	return (leter);
+}
