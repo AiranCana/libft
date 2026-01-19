b752984beda46d08fea1f39100df0598f6b49c44
AiranCana <airancc2005@gmail.com>
2026-01-19 16:20:04 +0100

    sifgo en ft_split

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: acanadil <acanadil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 15:48:08 by acanadil          #+#    #+#             */
-/*   Updated: 2026/01/19 16:04:39 by acanadil         ###   ########.fr       */
+/*   Updated: 2026/01/19 16:20:00 by acanadil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 char	*ft_strndup(const char *s, size_t n)
 {
-	
+	size_t	len;
+	void	*alloc;
+	char	*dest;
+
+	len = ft_strlen(s) + 1;
+	alloc = malloc(sizeof (char) * len);
+	dest = alloc;
+	while (*s)
+	{
+		*dest = *s;
+		++dest;
+		++s;
+	}
+	*dest = *s;
+	return (alloc);
 }
